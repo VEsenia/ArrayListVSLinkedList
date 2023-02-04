b45c30d13769d1e587ac08fbcaf835dd84cefb7a
esenia <chepkasovavv2@gmail.com>
2023-02-05 00:15:34 +0300

    Paste benchmark added

--- Main.java
+++ Main.java
@@ -5,59 +5,84 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-    private static final int amountOper = 1000000;
-    private static final int amountForAvearge = 1;
+    private static final int amountOper = 100;
+    private static final int amountForAverage = 5;
 
     private static ArrayList arrList;
     private static LinkedList lnkList;
     private static class TypeOper {
-        public static int paste = 2;
+        public static int add = 2;
         public static int delete = 1;
+        public static int paste = 3;
     }
 
-    private static int addOrDelElementsToList(List list, int operation) {
+    private static int addPasteOrDelElementsToList(List list, int operation) {
         int allSumDurationArrList = 0;
         LocalDateTime timeBeg;
         LocalDateTime timeEnd;
-        for (int j = 0; j < amountForAvearge; j++) {
-            list = new ArrayList();
-            timeBeg = LocalDateTime.now();
+        int averageSum = 0;
+        for (int j = 0; j < amountForAverage; j++) {
+            if(operation == TypeOper.add || operation == TypeOper.paste)
+                list.clear();
+            else
+                //Заполнить список перед удалением элементов
+                if((operation == TypeOper.delete) && list.isEmpty()) {
+                    for (int i = 0; i < amountOper; i++) {
+                        list.add(i);
+                    }
+                }
             for (int i = 0; i < amountOper; i++) {
-                if (operation == TypeOper.paste)
+                timeBeg = LocalDateTime.now();
+                if (operation == TypeOper.add)
                     list.add(i);
                 else
                 if (operation == TypeOper.delete)
-                    list.remove(i);
+                    list.remove(0);
+                else
+                if (operation == TypeOper.paste)
+                    //вставляем в середину списка
+                    list.add(list.size()/2, i);
+
+                timeEnd = LocalDateTime.now();
+                Duration duration = Duration.between(timeBeg, timeEnd);
+                allSumDurationArrList += duration.toMillis();
             }
-            timeEnd = LocalDateTime.now();
-            Duration duration = Duration.between(timeBeg, timeEnd);
-            allSumDurationArrList += duration.toMillis();
+            averageSum+=allSumDurationArrList;
+            allSumDurationArrList=0;
         }
-        return allSumDurationArrList;
+        return averageSum/amountForAverage;
     }
 
     private static void WriteInfo(String nameOperation, int arrListTime, int lnkListTime) {
         String str = nameOperation + " быстрее в %s в %d раз на %d мс";
         if (arrListTime > lnkListTime)
-            System.out.printf(str, "ArrayList", arrListTime / lnkListTime, arrListTime - lnkListTime);
+            System.out.printf(str, "ArrayList", (lnkListTime>0)?arrListTime/lnkListTime:0, arrListTime - lnkListTime);
         else
-            System.out.printf(str, "LinkedList", lnkListTime / arrListTime, lnkListTime - arrListTime);
+            System.out.printf(str, "LinkedList", (arrListTime>0)?lnkListTime/arrListTime:0, lnkListTime - arrListTime);
     }
 
     public static void main(String[] args) {
 
         //добавление элементов
         arrList = new ArrayList();
-        int allSumDurationArrList = addOrDelElementsToList(arrList, TypeOper.paste);
-
         lnkList = new LinkedList();
-        int allSumDurationLnkList = addOrDelElementsToList(lnkList, TypeOper.paste);
+        /*int allSumDurationArrList = addPasteOrDelElementsToList(arrList, TypeOper.add);
+
+        int allSumDurationLnkList = addPasteOrDelElementsToList(lnkList, TypeOper.add);
         WriteInfo("Добавление", allSumDurationArrList, allSumDurationLnkList);
 
+        System.out.println();
         //удаление элементов
-        allSumDurationArrList = addOrDelElementsToList(arrList, TypeOper.delete);
+        allSumDurationArrList = addPasteOrDelElementsToList(arrList, TypeOper.delete);
 
-        allSumDurationLnkList = addOrDelElementsToList(lnkList, TypeOper.delete);
+        allSumDurationLnkList = addPasteOrDelElementsToList(lnkList, TypeOper.delete);
         WriteInfo("Удаление", allSumDurationArrList, allSumDurationLnkList);
+*/
+        System.out.println();
+        //вставка элементов
+        int allSumDurationArrList = addPasteOrDelElementsToList(arrList, TypeOper.paste);
+
+        int allSumDurationLnkList = addPasteOrDelElementsToList(lnkList, TypeOper.paste);
+        WriteInfo("Вставка", allSumDurationArrList, allSumDurationLnkList);
     }
 }
